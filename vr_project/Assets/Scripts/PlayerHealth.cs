059d5e038b7036b3addb57eeecf3105500ac7de3
Jairo Rubio <rh.jairo.94@gmail.com>
2021-06-22 17:08:56 -0500

    Decrease health
    Health decreases upon triggerStay with objects with the "rain" tag and heals with objects with the "safeZone" tag

---
+++ vr_project/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float playerHealth = 100.0f;
+    float dmgMod = 1.0f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerStay (Collider c)
+    {
+        if (c.gameObject.tag == "Rain" && playerHealth > 0)
+        {
+            playerHealth -= dmgMod * Time.deltaTime;
+        }
+        else if (c.gameObject.tag == "SafeZone" && playerHealth < 100.0f)
+        {
+            playerHealth += dmgMod * Time.deltaTime;
+        }
+        else
+        {
+            playerHealth = 100.0f;
+        }
+    }
+}
--- vr_project/Assets/Scripts/VRWalk.cs
+++ vr_project/Assets/Scripts/VRWalk.cs
@@ -11,13 +11,11 @@ public class VRWalk : MonoBehaviour
 
     private CharacterController cc;
 
-    // Start is called before the first frame update
     void Start()
     {
         cc = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if(vrCamera.eulerAngles.x >= toggleAngle && vrCamera.eulerAngles.x < 90.0f)

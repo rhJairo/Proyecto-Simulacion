0bc83d12b445fb0ef634739569ae62b5e02a6876
Jairo Rubio <rh.jairo.94@gmail.com>
2021-06-28 17:31:58 -0500

    Handless interaction
    -Gaze controller script allows the user to have a handless interaction by hovering the reticle on the buttons for 2 seconds
    -Appropriate instructions for this feature have also been added

---
+++ vr_project/Assets/Scripts/GazeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+
+public class GazeController : MonoBehaviour
+{
+
+    public Image loadingImg;
+    public UnityEvent GVRClick;
+    public float totalTime = 2;
+    bool gvrStatus;
+    float gvrTimer;
+
+    void Update()
+    {
+        if (gvrStatus)
+        {
+            gvrTimer += Time.deltaTime;
+            loadingImg.fillAmount = gvrTimer / totalTime;
+        }
+
+        float rounded = (float)Math.Floor(gvrTimer);
+
+        print(rounded);
+        if (rounded == totalTime)
+        {
+            GVRClick.Invoke();
+            GVROff();
+        }
+    }
+
+    public void GVROn()
+    {
+        gvrStatus = true;
+    }
+
+    public void GVROff()
+    {
+        gvrStatus = false;
+        gvrTimer = 0;
+        loadingImg.fillAmount = 0;
+    }
+}
